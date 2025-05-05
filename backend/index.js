439c2f557e340ba96df9284285683d3156f2e887
redfyel <lianis.ee7@gmail.com>
2025-05-05 11:59:41 +0530

    dependcies

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,8 @@ const app = express();
 const PORT = 8888
 const allowedOrigins = [
   'http://localhost:5173',
-  'http://127.0.0.1:5173'
+  'http://127.0.0.1:5173',
+  'https://vibelytics.vercel.app/'
 ];
 app.use(cors({
   origin: function (origin, callback) {
